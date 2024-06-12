d9b8f6942870c273666bb74b95821a959ddfc63a
Michael <michael.gut.rob@gmail.com>
2024-06-12 07:55:49 -0500

    Primer commint

---
+++ 01_funciones.dart
@@ -0,0 +1,15 @@
+void main (List<String> args){
+//funciones
+//sin y con retorno 
+
+
+print('funciones!!');
+String name = 'Owlsmitch';
+mostrarMensaje(name);
+}
+
+mostrarMensaje(String nombre){
+  print('---------------------------');
+  print('Mensaje desde una función: $nombre');
+  print('--------------------------');
+}
